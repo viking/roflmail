937c700dc215b31f332f734e28de26815386e792
Jeremy Stephens <viking415@gmail.com>
2010-12-12 07:47:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roflmail::Application.config.secret_token = '849a7ce5a772d75d037818271d6480061c990a16ba3a201911d89870aa4b16fc5094abe98e9c9405c6d1c7b5121cc9f04b8dd6755bd8ed5205aaa885250694c6'
